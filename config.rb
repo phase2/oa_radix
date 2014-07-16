e14c9923356ce1c3cd33e8c8d51b82016e2705fa
Courtney Koppenhaver <ckoppenhaver@gmail.com>
2014-07-15 20:07:40 -0700

    OA-573 - Porting gems mixins and bootstrap to 3x

--- config.rb
+++ config.rb
@@ -1,5 +1,6 @@
 # Require any additional compass plugins here.
-require "compass_twitter_bootstrap";
+require 'bootstrap-sass';
+#require "compass_twitter_bootstrap";
 require "compass_radix";
 
 environment = :production
